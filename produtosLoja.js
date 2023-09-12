bccb92eb3a4fb54eec4e9b86c67c192023d428da
sarinhamanu <sarasejbrs@gmail.com>
2023-09-12 15:52:10 -0300

    atualizar

--- produtosLoja.js
+++ produtosLoja.js
@@ -1,22 +1,22 @@
 "use strict";
-class ProdutoLoja {
+class Produto {
     constructor(nome, preco) {
         this.nome = nome;
         this.preco = preco;
     }
 }
-class carinhoDeCompra {
+class carrinhoDeCompra {
     constructor() {
         this.lista = [];
     }
-    adicionarProduto(Produto) {
-        this.lista.push(Produto);
+    adicionarProduto(produto) {
+        this.lista.push(produto);
     }
     remover(produto) {
         this.lista = this.lista.filter((item) => item.nome !== produto.nome);
-        return "produto deletado";
+        return "produto removido";
     }
-    calcular() {
+    Calcular() {
         let soma = 0;
         this.lista.forEach(item => { soma = soma + item.preco; });
         //for(let i=0; i< this.lista.length; i++){
@@ -26,18 +26,18 @@ class carinhoDeCompra {
 }
 class Loja {
     constructor() {
-        this.estoqueProdutos = [];
-        this.carrinho = new carinhoDeCompra();
+        this.estoque = [];
+        this.carrinho = new carrinhoDeCompra();
     }
-    adicionarProdutoEstoque(produto) {
-        this.estoqueProdutos.push(produto);
+    adicionarProdutoLoja(produto) {
+        this.estoque.push(produto);
     }
-    removerProdutoEstoque(produto) {
-        this.estoqueProdutos = this.estoqueProdutos.filter(item => item.nome !== produto.nome);
-        return "Produto removido do estoque da loja";
+    remover(produto) {
+        this.estoque = this.estoque.filter((item) => item.nome !== produto.nome);
+        return "produto removido do estoque da loja";
     }
-    adicionarCarrinho(produto) {
-        const produtoEncontrado = this.estoqueProdutos.find(item => item.nome == produto.nome);
+    adicionarProdutoCarrinho(produto) {
+        const produtoEncontrado = this.estoque.find(item => item.nome == produto.nome);
         if (produtoEncontrado) {
             this.carrinho.adicionarProduto(produto);
             return "Produto adicionado ao carrinho de compras";
@@ -46,21 +46,36 @@ class Loja {
             return "Produto não encontrado no estoque";
         }
     }
-    removerProdutoDoCarrinho(produto) {
-        this.carrinho.remover(produto);
-        return "Produto removido do carrinho ";
+    RemoverProdutoCarrinho(produto) {
+        return this.carrinho.remover(produto);
+        //return "produto removido do carrinho de compras";
     }
-    exibirProduto(produto) {
+    exibirProduto() {
         console.log("exibir produto");
         for (let i = 0; i < this.carrinho.lista.length; i++) {
             console.log("exibir produto : " + this.carrinho.lista[i].nome + " tem o preço de: " + this.carrinho.lista[i].preco);
         }
     }
+    exibirValorCarrinho() {
+        return this.carrinho.Calcular();
+    }
 }
-let produtos1 = new ProdutoLoja("kit de maquiagem", 250);
-let produtos2 = new ProdutoLoja("kit de brinco", 200);
-let produtos3 = new ProdutoLoja("vestido", 250);
-const produtosNoCarrinho = new carinhoDeCompra();
-produtosNoCarrinho.adicionarProduto(produtos1);
-produtosNoCarrinho.adicionarProduto(produtos2);
-produtosNoCarrinho.adicionarProduto(produtos3);
+const produtos1 = new Produto("kit maquiagem", 250);
+const produtos2 = new Produto("kit brinco", 100);
+const produtos3 = new Produto("escova rotativa", 350);
+const produtosCarrinho = new Loja();
+produtosCarrinho.adicionarProdutoLoja(produtos1);
+produtosCarrinho.adicionarProdutoLoja(produtos2);
+produtosCarrinho.adicionarProdutoLoja(produtos3);
+console.log(produtosCarrinho.remover(produtos1));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos2));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.RemoverProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.exibirProduto());
+console.log(produtosCarrinho.exibirValorCarrinho());
+console.log(produtosCarrinho.remover(produtos2));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos2));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.RemoverProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.exibirProduto());
+console.log(produtosCarrinho.exibirValorCarrinho());
--- produtosLoja.ts
+++ produtosLoja.ts
@@ -1,6 +1,7 @@
-class ProdutoLoja{
+class Produto{
   nome: string;
   preco: number;
+ 
 
   constructor(nome: string, preco: number){
       this.nome = nome;
@@ -8,77 +9,115 @@ class ProdutoLoja{
   }
 }
 
-class carinhoDeCompra{
-  public lista: ProdutoLoja[] = [];
+class carrinhoDeCompra{
+   lista: Produto[] = [];
+ 
+  constructor(){
+
+  }
+ 
+  adicionarProduto(produto: Produto){
+      this.lista.push(produto);
+
+  }
+
+  remover(produto: Produto){
+
+      this.lista = this.lista.filter((item) => item.nome !== produto.nome);
+      return "produto removido"
+
+  }
+
+  Calcular(){
+      let soma =  0;
+      this.lista.forEach( item =>{soma = soma + item.preco});
+
+      //for(let i=0; i< this.lista.length; i++){
+      //   soma = soma + this.lista[i].preco;}
+      return soma;
+  }
+
+}
+
+class Loja{
+  private estoque:Produto[]=[];
+  carrinho = new carrinhoDeCompra();
 
   constructor(){
 
   }
 
-  adicionarProduto(Produto: ProdutoLoja){
-      this.lista.push(Produto);
+  adicionarProdutoLoja(produto: Produto){
+      this.estoque.push(produto);
+
   }
 
-  remover(produto:ProdutoLoja){
+  remover(produto: Produto){
 
-      this.lista = this.lista.filter((item) => item.nome !== produto.nome);
-      return "produto deletado"
-  }
-calcular(){
-  let soma =  0;
-        this.lista.forEach( item =>{soma = soma + item.preco});
+      this.estoque = this.estoque.filter((item) => item.nome !== produto.nome);
+      return "produto removido do estoque da loja";
 
-        //for(let i=0; i< this.lista.length; i++){
-        //   soma = soma + this.lista[i].preco;}
-        return soma;
-}
-
-}
-
-class Loja{
-  estoqueProdutos : ProdutoLoja [] = [];
-  carrinho = new carinhoDeCompra();
-
-  adicionarProdutoEstoque(produto: ProdutoLoja){
-    this.estoqueProdutos.push(produto);
   }
 
-  removerProdutoEstoque(produto:ProdutoLoja){
-    this.estoqueProdutos= this.estoqueProdutos.filter(item =>item.nome !== produto.nome);
-    return"Produto removido do estoque da loja"
+  adicionarProdutoCarrinho(produto: Produto){
+      const produtoEncontrado = this.estoque.find(
+          item => item.nome == produto.nome
+      );
+      if (produtoEncontrado){
+          this.carrinho.adicionarProduto(produto);
+          return "Produto adicionado ao carrinho de compras";
+      }
+      else{
+          return "Produto não encontrado no estoque";
+      }
+
   }
 
-  adicionarCarrinho(produto:ProdutoLoja){
+  RemoverProdutoCarrinho(produto: Produto){
+      return this.carrinho.remover(produto);
+      //return "produto removido do carrinho de compras";
+  }
 
-    const produtoEncontrado = this.estoqueProdutos.find(
-      item =>  item.nome == produto.nome
-    );
-    if(produtoEncontrado){
-   this.carrinho.adicionarProduto(produto);
-   return"Produto adicionado ao carrinho de compras";
+  exibirProduto(){
+      console.log("exibir produto")
+      for (let i=0; i< this.carrinho.lista.length; i ++){
+         console.log("exibir produto : "+this.carrinho.lista[i].nome+" tem o preço de: "+this.carrinho.lista[i].preco);
+       
+    }}
+
+    exibirValorCarrinho(){
+      return this.carrinho.Calcular();
     }
-    else{
-      return"Produto não encontrado no estoque";
-    }
-  }
-  removerProdutoDoCarrinho(produto:ProdutoLoja){
-    this.carrinho.remover(produto);
-    return"Produto removido do carrinho "
-  }
-  exibirProduto(produto:ProdutoLoja){
-    console.log("exibir produto")
-    for (let i=0; i< this.carrinho.lista.length; i ++){
-       console.log("exibir produto : "+this.carrinho.lista[i].nome+" tem o preço de: "+this.carrinho.lista[i].preco);
-      
-  }
-}
+
 }
 
-let produtos1 =new ProdutoLoja("kit de maquiagem", 250);
-    let produtos2 =new ProdutoLoja("kit de brinco",   200);
-    let produtos3 =new ProdutoLoja("vestido",   250);
-    
-    const produtosNoCarrinho= new carinhoDeCompra();
-    produtosNoCarrinho.adicionarProduto(produtos1);
-    produtosNoCarrinho.adicionarProduto(produtos2);
-    produtosNoCarrinho.adicionarProduto(produtos3);
+
+
+
+
+
+
+
+
+const produtos1 = new Produto("kit maquiagem", 250);
+const produtos2 = new Produto("kit brinco", 100);
+const produtos3 = new Produto("escova rotativa", 350);
+
+const produtosCarrinho = new Loja()
+produtosCarrinho.adicionarProdutoLoja(produtos1);
+produtosCarrinho.adicionarProdutoLoja(produtos2);
+produtosCarrinho.adicionarProdutoLoja(produtos3);
+
+console.log(produtosCarrinho.remover(produtos1));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos2));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.RemoverProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.exibirProduto());
+console.log(produtosCarrinho.exibirValorCarrinho());
+
+console.log(produtosCarrinho.remover(produtos2));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos2));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.RemoverProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.exibirProduto());
+console.log(produtosCarrinho.exibirValorCarrinho());
